efee2380326dea267f60b5f0b7a4e3c84ef04f8e
Aman Thakur <uttamthakur2999@gmail.com>
2021-05-11 12:49:43 +0530

    Basic Functions

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -1,6 +1,11 @@
 /*
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
+function sayHello(name) {
+  alert(`Hello ${name}`)
+  
+}
+sayHello(`Aman`)
 
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
@@ -10,7 +15,10 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
-
+function getFullName(firstName,lastName) {
+  return firstName+lastName
+}
+ let fullName = getFullName(`Aman`,`thakur`);
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
 the sum of both numbers. If user passes any data other then numbers alert a message saying `Enter Valid Input`
@@ -20,6 +28,10 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+function addTwoNumbers(firstnum ,secondNum) {
+  return  firstnum+secondNum
+}
+let sum=addTwoNumbers(35, 23);
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -32,6 +44,10 @@ calc(10, 20, 'add'); // 30
 calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
+// function calc(numA,numB, operation) {
+//   if()
+  
+// }
 
 
 /*
@@ -41,8 +57,31 @@ on if the year id leap year or not.
 isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
+function isLeapYear(year) {
+  if (year%400===0){
+    return true
+  }
+  else if (year%100===0){
+    return(false)
+  }
+  else if(year%4===0){
+    return true
+  }
+  else{
+    return false
+  }
 
+}
+isLeapYear(2000);
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+function getFactorial(num) {
+  let final=1
+  for(let i=num; i>=1; i--){
+    final *=i;
+  }
+  return final
+}
+getFactorial();
